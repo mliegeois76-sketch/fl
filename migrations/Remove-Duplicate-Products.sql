f280aa23be3bb16def931c97b37a7be3c532d09e
Mathis Liegeois <mathis@device-199.home>
2026-07-20 21:55:32 +0200

    Also remove products with null images

--- migrations/Remove-Duplicate-Products.sql
+++ migrations/Remove-Duplicate-Products.sql
@@ -2,3 +2,6 @@
 
 -- Delete the JPG versions (duplicates)
 delete from public.products where image_url in ('gorille.jpg', 'loutre.jpg', 'cheval.jpg', 'garcon.jpg', 'mouton.jpg', 'tigre.jpg', 'geisha.jpg');
+
+-- Also remove products with null images
+delete from public.products where image_url is null;
